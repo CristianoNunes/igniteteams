c1e61ad6dd5c6b9956de3ecc236958a94a22ba70
CristianoNunes <cristianonunes99@gmail.com>
2024-06-14 10:05:14 -0300

    Implemented component PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,12 +6,13 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(["Cristiano", "Pedro"]);
   return (
     <Container>
       <Header showBackButton />
@@ -39,6 +40,13 @@ export function Players() {
         />
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   );
 }
