73d3b1704d64428c9e3b2badd2ee4d71ab2a910d
CristianoNunes <cristianonunes99@gmail.com>
2024-07-18 08:35:38 -0300

    Add navigation types

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import {
   useFonts,
@@ -5,13 +6,11 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import theme from "@theme/index";
+import { Routes } from "@routes/index";
 
-import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
-import { StatusBar } from "react-native";
-import { NewGroup } from "@screens/NewGroup";
-import { Players } from "@screens/Players";
+
+import theme from "@theme/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +21,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
