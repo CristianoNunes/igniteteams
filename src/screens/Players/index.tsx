70a0e3bc2f00eecd18e8c55ffaddf378f3db3f34
CristianoNunes <cristianonunes99@gmail.com>
2024-07-22 19:47:28 -0300

    Adding player to group

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, Alert } from "react-native";
 import { useRoute } from "@react-navigation/native";
+
+import { AppError } from "@utils/AppError";
+
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroup } from "@storage/player/playersGetByGroup";
 
 import { Button } from "@components/Button";
 import { ButtonIcon } from "@components/ButtonIcon";
@@ -18,20 +23,53 @@ type RouteParams = {
 };
 
 export function Players() {
+  const route = useRoute();
+  const [newPlayerName, setNewPlayerName] = useState("");
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState([]);
 
-  const route = useRoute();
-
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert(
+        "Novo jogador",
+        "Informe o nome do jogador para adicionar."
+      );
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    };
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+
+      console.log(players);
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo jogador", error.message);
+      } else {
+        console.log(error);
+        Alert.alert("Novo jogador", "Não foi possível adicionar.");
+      }
+    }
+  }
 
   return (
     <Container>
       <Header showBackButton />
       <Highlight title={group} subtitle="adicione a galera e separe os times" />
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
-        <ButtonIcon icon="add" />
+        <Input
+          placeholder="Nome do jogador"
+          autoCorrect={false}
+          onChangeText={setNewPlayerName}
+        />
+
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
 
       <HeaderList>
@@ -61,11 +99,10 @@ export function Players() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[
           { paddingBottom: 100 },
-          !players.length && { flex: 1 },
+          players.length === 0 && { flex: 1 },
         ]}
       />
-
-      <Button title="Remover turma" type="SECONDARY" />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
