c77ce8d8a71ee7f8bc7ecb65a71c0c73e32a62f7
CristianoNunes <cristianonunes99@gmail.com>
2024-07-22 18:42:29 -0300

    Opening the selected group

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import { groupCreate } from "@storage/group/groupCreate";
+
+import { AppError } from "@utils/AppError";
 
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
@@ -16,10 +19,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma.");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+        console.log(error);
+      }
     }
   }
 
